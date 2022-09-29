1e6338861674ca3ddf84488c80cbd837c6c8a607
ahmadkal1171 <ahmadkal1171@gmail.com>
2022-09-29 15:15:11 +0800

    update

--- login.php
+++ login.php
@@ -21,7 +21,7 @@
                     width:100%;
                     height:100%;
                     object-fit:cover;
-                    opacity: 1;
+                    opacity: 0.8;
                 
                     /* background-image:url('backgroundlogin.jpg'); 
                     background-repeat:no repeat;
